7345401c13d0e1efa9fcb7a7556f99791a271332
helalsaeed2003 <helalsaeed2003@gmail.com>
2026-06-17 15:30:28 +0400

    Update ComponentTest.ino

--- Arduino Code/ComponentTest/ComponentTest.ino
+++ Arduino Code/ComponentTest/ComponentTest.ino
@@ -22,7 +22,7 @@
 #include <Servo.h>
 
 // ── Servo pins ───────────────────────────────────────────────────────────────
-#define SHOULDER_PIN  13
+#define SHOULDER_PIN  9
 #define ELBOW_PIN     10
 #define WRIST_PIN     11
 #define HAND_PIN      12
